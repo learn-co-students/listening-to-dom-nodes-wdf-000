ca98e1a433902017593c6a182bc5ed9962e2814b
Wendy Xu <guangw.xu@gmail.com>
2016-11-01 17:18:24 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0, l = array.length; i < l; i++) {
+    if (array[i] === stopValue) {
+      return array;
+      break;
+    }
+    array[i] = changeValue;
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0, l = array.length; i < l; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  for (var i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i];
+      break;
+    } 
+  }
+  return null;
+}
